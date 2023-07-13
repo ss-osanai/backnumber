670841a3ec7b5df2296227e5961b4e160aadb12f
Osanai <osanai@2nd-stage.jp>
2023-07-13 12:45:42 +0900

    Add commnet

--- new_post.ps1
+++ new_post.ps1
@@ -1,3 +1,11 @@
+<#
+    .SYNOPSIS
+        Hugo の記事ファイルを連番で作成します。
+        
+        $ .\new_post.ps1 7061 70
+
+        で、content/post ディレクトリに 7061.md から 7070.md までのファイルを作成します。
+#>
 param(
     [int]$start,
     [int]$end
