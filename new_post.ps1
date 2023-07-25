aa5d2c72227b42b2fb6bd7cf6398ad773d47e76a
Osanai <osanai@2nd-stage.jp>
2023-07-25 14:33:00 +0900

    Add sleep 0.5 sec

--- new_post.ps1
+++ new_post.ps1
@@ -2,7 +2,7 @@
     .SYNOPSIS
         Hugo の記事ファイルを連番で作成します。
         
-        $ .\new_post.ps1 7061 70
+        $ .\new_post.ps1 7061 7070
 
         で、content/post ディレクトリに 7061.md から 7070.md までのファイルを作成します。
 #>
@@ -14,4 +14,5 @@ param(
 for ($i = $start; $i -le $end; $i++) {
     # Write-Output $i
     hugo new post/$i.md
+    Start-Sleep -Seconds 1.5
 }
